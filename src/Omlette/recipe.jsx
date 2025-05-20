8304f4b41ad8421e2b3b169214d1ac5e10f58a29
SuriyaPrakash-05 <sur05012001.com>
2025-05-20 20:25:51 +0530

    recipe js

---
+++ src/Omlette/recipe.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+const recipe = () => {
+    return (
+        
+        <center><img src="./omlette image.jpeg" alt="Omlette" width ="550" height="300"/></center>
+        <h1><b><big>Simple Omlette Recipe</big></b></h1>
+        <p>An easy and quick dish,perfect for any meal.This classic omlette combines beaten eggs </br> cooked to perfection, optionally filled with choice of cheese, vegetables, or meats.</p > <hr>
+        <h3>Preparation Time</h3>
+        <ul>
+            <li><b>Total:</b> Approximately 10 minutes</li>
+            <li><b>Preparation:</b> 5 minutes</li>
+            <li><b>Cooking:</b> 5 minutes</li>
+        </ul><hr>
+            <h2><b>Ingredients</b></h2>
+            <ul>
+                <li>2-3 large eggs</li>
+                <li>Salt, to taste</li>
+                <li>Pepper, to taste</li>
+                <li>1 tablespoon of butter or oil</li>
+                <li>optional fillings: cheese,dicd vegetables,cooked meats,herbs</li>
+            </ul><hr>
+                <h2>Instructions</h2>
+                <ol>
+                    <li><b>Beat the eggs:</b> In a bowl, beat the egg with pinch of salt and pepper until they are </br> well mixed. You can add a tablespoon of water or milk for a fluffier texture.</li> </br>
+            <li><b>Heat the pan:</b> Place a non-stick frying pan over medium heat and add butter or oil.</li> </br>
+        <li><b>Cook the omlette:</b> Once the butter is melted and bubbling, pour in the eggs. Tilt the </br> pan to ensure the eggs evenly coat the surface.</li> </br >
+            <li><b>Add fillings(Optional):</b> When the eggs begin to set at the edges but are still slightly </br> runny in the middle, sprinkle your chosen filling over one half of the omlette</li > </br >
+                <li><b>Fold and serve:</b> As the omlette continues to cook, carefully lift one edge and fold it </br> over the fillings.Let cook for another minute, then slide it onto a plate.</li > </br
+                    < li > <b>Enjoy:</b>Serve hot, with additional salt and pepper if needed.</li >
+</ol > <hr>
+    <h2>Nutrition</h2>
+    <p>The table below shows nutritional value per serving without the additional fillings.</p>
+    <table border="1">
+        <tbody>
+            <tr>
+                <td>Calories</td>
+                <td><b>277kcal</b></td>
+            </tr>
+            <tr>
+                <td>Carbs</td>
+                <td><b>0g</b></td>
+            </tr>
+            <tr>
+                <td>Protein</td>
+                <td><b>20g</b></td>
+            </tr>
+            <tr>
+                <td>Fat</td>
+                <td><b>22g</b></td>
+            </tr>
+        </tbody>
+    </table>
+
+    )
+}
+export default recipe
